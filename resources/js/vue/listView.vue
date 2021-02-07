6e6a827ac4e96c06f5b66deda42d04c32d239954
Pedroenrick <pedro_penna22@hotmail.com>
2021-02-07 14:53:54 -0300

    Primeira versao Laravel

---
+++ resources/js/vue/listView.vue
@@ -0,0 +1,24 @@
+<template>
+    <div>
+        <div v-for="(item, index) in items" :key="index" >
+            <list-item :item="item" class="item-large" v-on:itemchanged="$emit('reloadlist')" />
+        </div>
+    </div>
+</template>
+
+<script>
+import listItem from './listItem';
+export default {
+    props: ['items'],
+    components: {
+        listItem
+    }
+}
+</script>
+
+<style scoped>
+.item-large{
+    background: #e6e6e6;
+    padding: 5px;
+}
+</style>
